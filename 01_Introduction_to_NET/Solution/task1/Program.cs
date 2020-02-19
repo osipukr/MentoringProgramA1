c1319c662f2adda13a42e69da3ea14a6be1ed8c1
Raman Asipuk <raman.asipuk@thomsonreuters.com>
2020-02-19 18:45:41 +0300

    Completed module 01

---
+++ 01_Introduction_to_NET/Solution/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace task1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var timeService = new TimeService();
+
+            Console.WriteLine(timeService.GetCurrentTime());
+        }
+    }
+}
